5d410ed9679bc3a6c29d850e7e377079d3d181b3
Codigoquatro <codigoquatro2022@gmail.com>
2023-12-25 10:53:50 -0300

    nova pasta usuario

--- ponto/leitura_qrcode.php
+++ ponto/leitura_qrcode.php
@@ -1,22 +1,51 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Instascan</title>
-    <script type="text/javascript" src="instascan.min.js"></script> 
-  </head>
-  <body>
-    <video id="preview"></video>
-    <script type="text/javascript">
-      var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-      scanner.addListener('scan', function (content, image) {
-        console.log(content);
-      });
- 
-      Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-          scanner.start(cameras[0]);
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leitor de QR Code</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f4f4f4;
         }
-      });
-    </script> 
-  </body>
+
+        #preview {
+            width: 100%;
+            max-width: 400px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div id="preview"></div>
+
+    <script src="../ponto/instascan.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+
+            scanner.addListener('scan', function (content) {
+                // Redireciona para o arquivo PHP com o conteúdo lido do QR code
+                window.location.href = 'caminho/para/seu/arquivo.php?conteudo=' + encodeURIComponent(content);
+            });
+
+            Instascan.Camera.getCameras().then(function (cameras) {
+                if (cameras.length > 0) {
+                    scanner.start(cameras[0]);
+                } else {
+                    console.error('Nenhuma câmera encontrada.');
+                    alert('Nenhuma câmera encontrada.');
+                }
+            }).catch(function (e) {
+                console.error(e);
+                alert('Erro ao acessar a câmera: ' + e);
+            });
+        });
+    </script>
+</body>
 </html>
